5c94e83f5eea49b9c87972b05c70d70cd28b780d
Jaycee <vsquare396@gmail.com>
2018-04-09 00:00:39 -0700

    Initial commits

--- app/src/main/java/bankzworld/com/activity/MainActivity.java
+++ app/src/main/java/bankzworld/com/activity/MainActivity.java
@@ -4,15 +4,14 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.app.FragmentManager;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -22,8 +21,8 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import bankzworld.com.fragment.MainActivityFragment;
 import bankzworld.com.R;
+import bankzworld.com.fragment.MainActivityFragment;
 import bankzworld.com.network.NetworkClass;
 import butterknife.ButterKnife;
 
@@ -172,11 +171,7 @@ public class MainActivity extends AppCompatActivity
             } else {
                 deleteAccount();
             }
-
-        } else if (id == R.id.nav_send) {
-            Log.d(TAG, "onNavigationItemSelected: clicked");
         }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
